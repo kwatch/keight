f9138ae7377ead92b0406bc53defffeb186240f4
makoto kuwata <kwa@kuwata-lab.com>
2010-10-09 21:18:44 +0900

    ruby: implement 'Request' class to parse query string.

--- lib/keight/core.rb
+++ lib/keight/core.rb
@@ -13,14 +13,43 @@ module K8
   class Request
 
     def initialize(env)
-      return unless env
+      init(env) if env
+    end
+
+    def init(env)
       @env = env
       @method       = (s = env['REQUEST_METHOD']) ? s.intern : s
       @query_string = env['QUERY_STRING']
       @uri          = env['REQUEST_URI']
       @path         = (@uri || '').sub(/\?.*/, '')
+      #
+      parse_request()
     end
+
     attr_accessor :env, :method, :query_string, :uri, :path
+    attr_accessor :params, :files
+
+    private
+
+    def parse_request
+      case @method
+      when :GET, :HEAD
+        @params, @files = parse_query_string()
+      when :POST, :PUT, :DELETE
+        @params, @files = parse_multipart()
+      else
+        @params = {}
+        @files = {}
+      end
+    end
+
+    def parse_query_string
+      return K8::Util::parse_query_string(@query_string || ''), {}
+    end
+
+    def parse_multipart
+      return K8::MultPart::parse(@input)
+    end
 
   end
 

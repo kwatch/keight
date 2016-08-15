0af69db7f6fc53b831167d1c46bbb36502cc8481
makoto kuwata <kwa@kuwata-lab.com>
2016-08-15 21:05:00 +0900

    ruby: rename '_validate_action_method_existence()' to 'validate_action_method_existence()'

--- lib/keight.rb
+++ lib/keight.rb
@@ -1495,7 +1495,7 @@ module K8
           buf2 = []
           action_class._mappings.each do |upath, action_methods|
             #; [!ue766] raises error when action method is not defined in action class.
-            _validate_action_method_existence(action_class, action_methods)
+            validate_action_method_existence(action_class, action_methods)
             #
             full_urlpath = "#{curr_urlpath}#{upath}"
             if has_urlpath_param?(full_urlpath)
@@ -1560,7 +1560,7 @@ module K8
     end
 
     ## raises error when action method is not defined in action class
-    def _validate_action_method_existence(action_class, action_methods)
+    def validate_action_method_existence(action_class, action_methods)
       action_methods.each do |req_meth, action_method_name|
         action_class.method_defined?(action_method_name)  or
           raise UnknownActionMethodError.new("#{req_meth.inspect}=>#{action_method_name.inspect}: unknown action method in #{action_class}.")

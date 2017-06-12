f33b49f0851d35a969e270f4511fcde8a03158f3
kwatch <kwatch@gmail.com>
2017-06-12 22:45:19 +0900

    python: define 'help', 'package' and 'clean' tasks

--- Kookbook.py
+++ Kookbook.py
@@ -1,5 +1,51 @@
+# -*- coding: utf-8 -*-
+
 import sys, os, re
 from kook.utils import glob2
+
+kookbook.default = 'help'
+
+
+@recipe
+def task_help(self):
+    print(r"""
+How to release:
+
+  $ git checkout python-dev
+  $ git checkout -b python-1.0
+  $ kk edit 1.0.0
+
+  ### create package
+  $ kk package
+  $ ls dist
+  cdnget-1.0.0.tar.gz
+
+  ### confirm package
+  $ bash
+  $ pyvenv testenv
+  $ . testenv/bin/activate
+  $ pip install dist/keight-1.0.0.tar.gz
+  $ python
+  >>> import keight
+  >>> keight.__release__
+  1.0.0
+  >>> exit
+  $ deactivate
+  $ rm -r testenv
+
+  ### commit and tag
+  $ git add -p
+  $ git commit -m "python: release preparation for 1.0.0"
+  $ git tag py-1.0.0
+  $ git push -u origin python-1.0
+  $ git push --tags
+
+  ### reset 'python' branch
+  $ git checkout python
+  $ git reset --hard python-1.0
+  $ git push -uf origin python
+  $ git checkout python-dev
+""")
 
 
 @recipe
@@ -28,6 +74,44 @@ def task_test(c, *args, **kwargs):
             system_f(c%"bash test-all.sh")
         finally:
             rm_f("test-all.sh")
+
+@recipe
+def task_edit(c, release=None):
+    """edit files"""
+    if release is None:
+        print("*** ERROR: edit task requires release number.")
+        return 1
+    def filenames():
+        system("python setup.py sdist --manifest-only > /dev/null 2>&1")
+        with open('MANIFEST') as f:
+            for line in f:
+                if line.startswith('#'):
+                    continue
+                yield line.strip()
+    #
+    for fname in filenames():
+        print("$ edit %s" % fname)
+        with open(fname, 'r+', encoding='utf-8') as f:
+            s = f.read()
+            s = re.sub(r'\$[R]elease\$', release, s)
+            s = re.sub(r'\$[R]elease: .*?\$', '$''Release: %s $' % release, s)
+            s = re.sub(r'\$[R]elease: .*?\$', '$''Release: %s $' % release, s)
+            #
+            f.seek(0)
+            f.truncate()
+            f.write(s)
+
+@recipe
+def task_package(c):
+    """create package"""
+    system(c%"python setup.py sdist")
+
+@recipe
+def task_clean(c):
+    """remove files"""
+    rm_f("**/*.pyc")
+    rm_rf("MANIFEST")
+    rm_rf("dist", "build")
 
 
 @recipe
